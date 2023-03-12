0757552ab77b5b02c034cb77961b50a47d3cff03
ryanahsu <ryanahsu@outlook.com>
2023-03-12 00:28:34 -0800

    Cleaned up code design

--- ipm-app/src/pages/UploadPage.js
+++ ipm-app/src/pages/UploadPage.js
@@ -1,42 +1,24 @@
 import React, { useState } from "react";
 
 export function TextSubmissionForm() {
-  const [q1, setQ1] = useState("");
-  const [q2, setQ2] = useState("");
-  const [q3, setQ3] = useState("");
-  const [q4, setQ4] = useState("");
-  const [q5, setQ5] = useState("");
+  const [className, setClassName] = useState("");
+  const [projDescription, setProjDescription] = useState("");
+  const [tagNames, setTagNames] = useState("");
+  const [fileUpload, setFileUpload] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (q1.trim() === "" || q2.trim() === "" || q3.trim() === "" || (q4.trim() === "" && q5.trim() === "")) {
+    if (className.trim() === "" || projDescription.trim() === "" || tagNames.trim() === "" || fileUpload.trim() === "") {
       alert("Please fill out all required fields.");
     } else {
-      // Do something with the submitted text, e.g. send it to a server
+      // Do something with the submitted text
       alert("Thank you for submitting!");
       // Reset the form after submission
-      setQ1("");
-      setQ2("");
-      setQ3("");
-      setQ4("");
-      setQ5("");
+      setClassName("");
+      setProjDescription("");
+      setTagNames("");
+      setFileUpload("");
     }
-  }
-
-  function handleQ1Change(event) {
-    setQ1(event.target.value);
-  }
-  function handleQ2Change(event) {
-    setQ2(event.target.value);
-  }
-  function handleQ3Change(event) {
-    setQ3(event.target.value);
-  }
-  function handleQ4Change(event) {
-    setQ4(event.target.value);
-  }
-  function handleQ5Change(event) {
-    setQ5(event.target.value);
   }
 
   return (
@@ -46,26 +28,19 @@ export function TextSubmissionForm() {
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label className="form-label">What class are you uploading for?</label>
-                        <input className="form-control" value={q1} onChange={handleQ1Change} ></input>
+                        <input className="form-control" value={className} onChange={(event) => (setClassName(event.target.value))} required></input>
                     </div>
                     <div className="mb-3">
                         <label>Give a brief description of your project.</label>
-                        <textarea className="form-control" rows="6" value={q2} onChange={handleQ2Change} required></textarea>
+                        <textarea className="form-control" rows="6" value={projDescription} onChange={(event) => (setProjDescription(event.target.value))} required></textarea>
                     </div>
                     <div className="mb-3">
                         <label>Add any tags that are associated with your project.</label>
-                        <textarea className="form-control" rows="6" value={q3} onChange={handleQ3Change} required></textarea>
+                        <textarea className="form-control" rows="6" value={tagNames} onChange={(event) => (setTagNames(event.target.value))} required></textarea>
                     </div>
                     <div className="mb-3">
                         <label for="formFile" className="form-label">Upload your project here</label>
-                        <input className="form-control" type="file" id="formFile" value={q4} onChange={handleQ4Change} ></input>
-                    </div>
-                    <div className="or">
-                        <p>or</p>
-                    </div>
-                    <div className="mb-3 mt-0">
-                        <label className="form-label"></label>
-                        <input className="form-control" placeholder="Paste a link" value={q5} onChange={handleQ5Change} ></input>
+                        <input className="form-control" type="file" id="formFile" value={fileUpload} onChange={(event) => (setFileUpload(event.target.value))} required></input>
                     </div>
                     <div>
                         <button type="submit" className="btn btn-primary mb-3">Submit</button>
