88450d5148856dac0c7b6d1d51b9b0bad52c55cf
ryanahsu <ryanahsu@outlook.com>
2023-03-13 21:56:33 -0700

    Minor styling changes

--- ipm-app/src/pages/homepage.js
+++ ipm-app/src/pages/homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/NavBar';
-import {LoadProjectProfile} from '../pages/profilepage'
-import { getStorage, ref as storageRef, uploadBytes } from "firebase/storage";
+import {LoadProjectProfile} from '../pages/profilepage';
+
 
 export function Homepage() {
   return (
@@ -16,15 +16,18 @@ export function Homepage() {
         </header>
         <main>
             <div>
-                <img class="bg-image" alt="" src="../img/ratul-ghosh-AlXdGRZ7x5k-unsplash.jpg"></img>
+                <img className="bg-image" alt="" src="../img/ratul-ghosh-AlXdGRZ7x5k-unsplash.jpg"></img>
             </div>
-            <div className="container-home">
-                    <div className="about">
-                        <h2>What is IPM?</h2>
-                        <p>IPM is a centralized hub for informatics students at the University of Washington to share projects completed within the iSchool's various classes to offer insight about the class material and teachings, and insipration to create meaningful work</p>
-                        <p>Disclaimer: IPM is not a tool for plagerism and users should only use sources as inspiration and as a guide for what informatics classes offer</p>
+            <div>
+            <div className="flex-box">
+                <div className="about">
+                    <h2>What is IPM?</h2>
+                    <p>IPM is a centralized hub for informatics students at the University of Washington to share projects completed within the iSchool's various classes to offer insight about the class material and teachings, and insipration to create meaningful work</p>
+                    <p>Disclaimer: IPM is not a tool for plagerism and users should only use sources as inspiration and as a guide for what informatics classes offer</p>
                 </div>
             </div>
+            </div>
+
         </main>
             
         <footer>
